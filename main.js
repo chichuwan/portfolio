5c3568b3ea604e015cbfff4601042771ff5cd3f7
chichuwan <112555157+chichuwan@users.noreply.github.com>
2023-10-05 12:16:08 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typeingEffect = new Typed(".multiText", {
+    strings : ["Antonio", "a Web Developer","a CS Student"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 1500
+})
